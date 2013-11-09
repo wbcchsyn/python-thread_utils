568d7ca49cc7f351151db4daad68a8808884cc70
Yoshida Shin <wbcchsyn@gmail.com>
2013-11-09 18:27:03 +0900

    Make tests runnable from every directory.

--- src/tests/test_background.py
+++ src/tests/test_background.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.pathabspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import pytest
 import threading
