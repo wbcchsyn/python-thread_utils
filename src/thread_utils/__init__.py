e0aaaf49f722b198aea389609a4f8a90784401d6
Yoshida Shin <wbcchsyn@gmail.com>
2014-09-07 02:05:42 +0000

    Quit to use __import__ to support both 2.x and 3.x. 2to3 will do.

--- src/thread_utils/__init__.py
+++ src/thread_utils/__init__.py
@@ -16,15 +16,7 @@ limitations under the License.
 '''
 
 
-import sys
-
-
-# To support 2.x and 3.x, use __import__.
-synchronized = __import__('synchronized', globals(), locals(),
-                          ['synchronized'], 1).synchronized
-async = __import__('async', globals(), locals(), ['async'], 1).async
-__errors = __import__('error', globals(), locals(), [], 1)
-Error = __errors.Error
-TimeoutError = __errors.TimeoutError
-DeadPoolError = __errors.DeadPoolError
-Pool = __import__('pool', globals(), locals(), ['Pool'], 1).Pool
+from error import TimeoutError, DeadPoolError
+from synchronized import synchronized
+from async import async
+from pool import Pool
